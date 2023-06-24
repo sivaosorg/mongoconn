978938771d0ee815376dc0ace3ccc2798b53f003
pnguyen215 <nguyenhuuphuocit97@gmail.com>
2023-06-24 21:31:53 +0700

    :sparkles: feat: added mongodb connection #1

--- go.mod
+++ go.mod
@@ -1,3 +1,33 @@
 module github.com/sivaosorg/mongodbconn
 
 go 1.20
+
+require (
+	github.com/sivaosorg/govm v1.0.4
+	go.mongodb.org/mongo-driver v1.12.0
+)
+
+require (
+	github.com/BurntSushi/toml v1.3.2 // indirect
+	github.com/fatih/color v1.15.0 // indirect
+	github.com/golang/snappy v0.0.1 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/klauspost/compress v1.13.6 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
+	github.com/natefinch/lumberjack v2.0.0+incompatible // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.1.2 // indirect
+	github.com/xdg-go/stringprep v1.0.4 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
+	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
+	golang.org/x/sync v0.0.0-20220722155255-886fb9371eb4 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/text v0.7.0 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
--- mongodbconn/mongodbconn.go
+++ mongodbconn/mongodbconn.go
@@ -1,1 +1,87 @@
 package mongodbconn
+
+import (
+	"context"
+	"fmt"
+	"sync"
+
+	"github.com/sivaosorg/govm/dbx"
+	"github.com/sivaosorg/govm/logger"
+	"github.com/sivaosorg/govm/mongodb"
+	"github.com/sivaosorg/govm/utils"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+var (
+	instance *mongo.Client
+	_logger  = logger.NewLogger()
+)
+
+func NewClient(config mongodb.MongodbConfig) (*mongo.Client, dbx.Dbx) {
+	s := dbx.NewDbx().SetDatabase(config.Database)
+	if !config.IsEnabled {
+		s.SetConnected(false).
+			SetMessage("Mongodb unavailable").
+			SetError(fmt.Errorf(s.Message))
+		return &mongo.Client{}, *s
+	}
+	if instance != nil {
+		s.SetConnected(true).SetNewInstance(false)
+		return instance, *s
+	}
+	if config.TimeoutSecondsConn <= 0 {
+		config.SetTimeoutSecondsConn(10)
+	}
+	if config.AllowConnSync {
+		// Used to execute client creation procedure only once.
+		var mongoOnce sync.Once
+		mongoOnce.Do(func() {
+			_instance, state := getConn(config, instance, s)
+			instance = _instance
+			s = &state
+		})
+	} else {
+		_instance, state := getConn(config, instance, s)
+		instance = _instance
+		s = &state
+	}
+	if config.DebugMode {
+		_logger.Info(fmt.Sprintf("Mongodb client connection:: %s", config.Json()))
+		if s.IsConnected {
+			_logger.Info(fmt.Sprintf("Connected successfully to mongodb:: %s (database: %s)", getUrlConn(config), config.Database))
+		}
+	}
+	return instance, *s
+}
+
+func getUrlConn(config mongodb.MongodbConfig) string {
+	if utils.IsNotEmpty(config.UrlConn) {
+		return config.UrlConn
+	}
+	if utils.IsNotEmpty(config.Username) && utils.IsNotEmpty(config.Password) {
+		form := fmt.Sprintf("mongodb://%s:%s@%s:%d/%s", config.Username, config.Password, config.Host, config.Port, config.Database)
+		return form
+	}
+	form := fmt.Sprintf("mongodb://%s:%d/%s", config.Host, config.Port, config.Database)
+	return form
+}
+
+func getConn(config mongodb.MongodbConfig, instance *mongo.Client, s *dbx.Dbx) (*mongo.Client, dbx.Dbx) {
+	_options := options.Client().ApplyURI(getUrlConn(config))
+	client, err := mongo.Connect(context.Background(), _options)
+	if err != nil {
+		s.SetConnected(false).SetError(err).SetMessage(err.Error())
+		instance = nil
+		return instance, *s
+	}
+	if err := client.Ping(context.Background(), readpref.Primary()); err != nil {
+		s.SetConnected(false).SetError(err).SetMessage(err.Error())
+		instance = nil
+		return instance, *s
+	}
+	s.SetConnected(true).SetNewInstance(true).SetMessage("Connection established")
+	instance = client
+	return instance, *s
+}
